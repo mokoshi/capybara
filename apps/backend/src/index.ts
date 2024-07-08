d3966026c1fe9c10276082440b15451ee0fc8f7f
mokoshi <r.koshi.0514@gmail.com>
2024-07-08 16:02:36 +0900

    claudeと接続

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,36 @@
-import { Hono } from 'hono'
+import Anthropic from "@anthropic-ai/sdk";
+import { Hono } from "hono";
 
-const app = new Hono()
+type Bindings = {
+  CLAUDE_API_KEY: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/", async (c) => {
+  const claudeAPIKey = c.env.CLAUDE_API_KEY;
+
+  const anthropic = new Anthropic({ apiKey: claudeAPIKey });
+
+  const msg = await anthropic.messages.create({
+    model: "claude-3-5-sonnet-20240620",
+    max_tokens: 1000,
+    temperature: 0,
+    system: "Respond only with short poems.",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Why is the ocean salty?",
+          },
+        ],
+      },
+    ],
+  });
+
+  return c.text(JSON.stringify(msg));
+});
+
+export default app;
